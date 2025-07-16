2e1aa0c052dbe8d92660b60ef8a87142caf40286
Kishan Kushwaha <108648818+kishankushwahaa@users.noreply.github.com>
2025-07-16 11:44:24 +0000

    data types

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,14 @@
+let name = "Kishan"
+let age = 24
+let isLoggedIn = false
+
+
+// number => 2 to power 53 range
+// bigint
+//string =>""
+//boolean => true/false  
+// null => standalone value/empty
+//undefined => value not assigned
+// symbol => unique
+
+console.log(typeof null);
